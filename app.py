578f03407ed40635654223ac6bf1c12e543470f4
raahelpie <17131a05h5@gvpce.ac.in>
2020-03-20 00:51:22 +0530

    more data and design

--- app.py
+++ app.py
@@ -12,10 +12,19 @@ df_bar_day2day = df_day2day.head(25)
 trace_cases = go.Scatter(
     x=df_bar_day2day['Date'],
     y=df_bar_day2day['Cases'],
-    name="Cases",
+    name="Total",
     mode="lines+markers",
     marker=dict(size=10, opacity=0.5),
     line=dict(color='#17BECF'),
+    opacity=0.9
+)
+trace_active = go.Scatter(
+    x=df_bar_day2day['Date'],
+    y=df_bar_day2day['Active'],
+    name="Active",
+    mode="lines+markers",
+    marker=dict(size=10, opacity=0.5),
+    line=dict(color='#FFA500'),
     opacity=0.9
 )
 trace_deaths = go.Scatter(
@@ -37,8 +46,8 @@ trace_recoveries = go.Scatter(
     opacity=0.9
 )
 
-data1 = [trace_cases, trace_deaths, trace_recoveries]
-layout1 = dict(title="Day to Day Analysis Of Coronavirus in India")
+data1 = [trace_cases, trace_active, trace_deaths, trace_recoveries]
+layout1 = dict(title="Date-wise statistics of COVID-19, India")
 
 fig1 = dict(data=data1, layout=layout1)
 
@@ -48,26 +57,37 @@ df_new_cvirus = df_cvirus.head(20)
 
 trace1 = go.Bar(
     x=df_new_cvirus['state_name'],
-    y=df_new_cvirus['total_cases'],
-    name="Confirmed"
+    y=df_new_cvirus['active_cases'],
+    name="Active",
+    marker=dict(color="#FFA500"),
+    opacity=0.7,
+    marker_line_width=0.5,
+    marker_line_color='rgb(0, 0, 0, 0.6)',
 )
 trace2 = go.Bar(
     x=df_new_cvirus['state_name'],
     y=df_new_cvirus['total_cured'],
-    name="Recovered"
+    name="Recovered",
+    marker=dict(color="#00FF00"),
+    marker_line_width=0.5,
+    marker_line_color='rgb(0, 0, 0, 0.6)',
 )
 trace3 = go.Bar(
     x=df_new_cvirus['state_name'],
     y=df_new_cvirus['total_deaths'],
-    name="Deaths"
+    name="Deaths",
+    marker=dict(color="#FF0000"),
+    marker_line_width=0.5,
+    marker_line_color='rgb(0, 0, 0, 0.6)',
 )
 data2 = [trace1, trace2, trace3]
 colors = ["blue", "green", "red"]
-layout2 = go.Layout(
-    barmode="group"
+layout2 = dict(
+    title="State-wise statistics of COVID-19",
+    barmode="stack",
 )
 
-fig2 = go.Figure(data=data2, layout=layout2)
+fig2 = dict(data=data2, layout=layout2)
 
 external_stylesheets = ['https://codepen.io/criddyp/pen/bWLwgP.css']
 
@@ -83,11 +103,11 @@ app.layout = html.Div([
     html.Div([
         dcc.Graph(id="Daily Trend",
                   figure=fig1)
-    ], className="okay okay1"),
+    ], className="okay okay1 grow"),
     html.Div([
         dcc.Graph(id="Bar Graph",
                   figure=fig2)
-    ], className="okay okay2"),
+    ], className="okay okay2 grow"),
 ])
 
 
